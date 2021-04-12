d1de8cd8be648accda2cdf7b3d2666404f6289a3
Chris Mellard <chris.mellard@icloud.com>
2021-04-12 13:42:11 +1200

    minor tweak

--- src/svg-external.tsx
+++ src/svg-external.tsx
@@ -1,10 +1,3 @@
-import * as React from 'react'
 import {ReactComponent as Chevron} from './chevron.svg'
 
-const SvgComponent = () => {
-    return (
-        <Chevron/>
-    )
-}
-
-export default SvgComponent
+export default Chevron
